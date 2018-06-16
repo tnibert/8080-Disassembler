9d7dc2e77c9d5e7c3e983611ce40099997b8e125
Tim Nibert <docz2a@gmail.com>
2018-06-16 16:46:29 +1000

    added includes and more opcodes

--- disassembler.c
+++ disassembler.c
@@ -1,27 +1,36 @@
-/*    
-    *codebuffer is a valid pointer to 8080 assembly code    
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    *codebuffer is a valid pointer to 8080 assembly code
     pc is the current offset into the code
 
-    returns the number of bytes of the op    
+    returns the number of bytes of the op
 */
 
-int Disassemble8080Op(unsigned char *codebuffer, int pc)    
+int Disassemble8080Op(unsigned char *codebuffer, int pc)
 {
     unsigned char *code = &codebuffer[pc];
     int opbytes = 1;
     printf ("%04x ", pc);
     switch (*code)
     {
-        case 0x00: printf("NOP"); break;    
-        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
-        case 0x02: printf("STAX   B"); break;    
-        case 0x03: printf("INX    B"); break;    
-        case 0x04: printf("INR    B"); break;    
-        case 0x05: printf("DCR    B"); break;    
-        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
-        case 0x07: printf("RLC"); break;    
-        case 0x08: printf("NOP"); break;    
-        /* ........ */    
+        case 0x00: printf("NOP"); break;
+        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+        case 0x02: printf("STAX   B"); break;
+        case 0x03: printf("INX    B"); break;
+        case 0x04: printf("INR    B"); break;
+        case 0x05: printf("DCR    B"); break;
+        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;
+        case 0x07: printf("RLC"); break;
+        case 0x08: printf("NOP"); break;
+        case 0x09: printf("DAD    B"); break;
+        case 0x0a: printf("LDAX   B"); break;
+        case 0x0b: printf("DCX    B"); break;
+        case 0x0c: printf("INR    C"); break;
+        case 0x0d: printf("DCR    C"); break;
+        case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes=2; break;
+        /* ........ */
         case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;    
         /* ........ */    
         case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;    
